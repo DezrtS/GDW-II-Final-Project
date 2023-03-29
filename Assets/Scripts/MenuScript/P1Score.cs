e2de50712262d25c6e686b3215525856a236d5d8
ChristianZambri777 <christian.zambri@ontariotechu.net>
2023-03-29 12:46:43 -0400

    Added Score System Prototype and Tutorial Scenes

--- Assets/Scripts/MenuScript/P1Score.cs
+++ Assets/Scripts/MenuScript/P1Score.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class P1Score : Singleton<P1Score>
 {
     int score = 0;
+    bool once = true;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (score == 3 && once)
+        {
+            SceneManager.LoadScene("VictoryRed");
+            once = false;
+        }
     }
 
 
