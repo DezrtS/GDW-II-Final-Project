990a5cf1208cae875da1e38a4de3d6b4e7a79e98
Denzil Walrond <denzil@walrond.me>
2023-04-05 15:01:28 -0400

    Made Tweaks to Final Animation

--- Assets/Scripts/DenzilGame/VictoryManager.cs
+++ Assets/Scripts/DenzilGame/VictoryManager.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictoryManager : MonoBehaviour
 {
+    [SerializeField] private bool redWon;
+
+    [SerializeField] private GameObject star;
+    [SerializeField] private Color redColor;
+    [SerializeField] private Color blueColor;
+
+    private bool isReplaying = false;
+
+    private void OnDestroy()
+    {
+        TransitionManager.Instance.OnTransitionEnded -= OnTransitionEnded;
+    }
+
+    private void OnTransitionEnded(bool isExitTransition)
+    {
+        if (isReplaying && !isExitTransition)
+        {
+            P1Score.Instance.ResetScore();
+            P2Score.Instance.ResetScore();
+            LoadMainMenuScene();
+        }
+    }
+
     void Start()
     {
+        TransitionManager.Instance.OnTransitionEnded += OnTransitionEnded;
+        if (redWon)
+        {
+            star.GetComponent<SpriteRenderer>().color = redColor;
+        } else
+        {
+            star.GetComponent<SpriteRenderer>().color = blueColor;
+        }
         StartCoroutine(StartMusic());
     }
 
@@ -14,5 +46,21 @@ public class VictoryManager : MonoBehaviour
         yield return new WaitForSeconds(2);
         StartCoroutine(SoundManager.Instance.fadeTitleMusicSoundIn());
         ConfettiManager.Instance.OnlyPlayConfetti();
+    }
+
+    public void ReplayGame()
+    {
+        if (!isReplaying)
+        {
+            isReplaying = true;
+            TransitionManager.Instance.PlayRandomEnterTransition();
+        }
+        //  Destroy(FindObjectOfType<SoundManager>());
+        //Destroy(GameObject.Find("Sound"));
+    }
+
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("GameMenu");
     }
 }
