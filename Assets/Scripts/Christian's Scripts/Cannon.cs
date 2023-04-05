94fc6282f98e9abc2a4a1e3e37159a1527ba5dcc
Denzil Walrond <denzil@walrond.me>
2023-04-05 16:31:13 -0400

    Added Cannon Fire Particle Effects to Bullet Buttons

--- Assets/Scripts/Christian's Scripts/Cannon.cs
+++ Assets/Scripts/Christian's Scripts/Cannon.cs
@@ -11,6 +11,9 @@ public class Cannon : MonoBehaviour
     public GameObject bulletPre;
     public float bulletSpeed;
     public Vector2 distance;
+
+    [SerializeField] private ParticleSystem cannonFire;
+
     [SerializeField] GameObject player1;
 
 
@@ -65,6 +68,21 @@ public class Cannon : MonoBehaviour
 
         distanceP2 = new Vector2(player2.transform.position.x - spawn.position.x, player2.transform.position.y - spawn.position.y);
 
+        Vector2 direction;
+
+        if (target == 1)
+        {
+            direction = distance;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        } else if (target == 2)
+        {
+            direction = distanceP2;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+
         if (fireTimer.UpdateTimer())
         {
             if (target == 1)
@@ -72,6 +90,8 @@ public class Cannon : MonoBehaviour
                 SoundManager.Instance.playShootSound();
                 var bullet = Instantiate(bulletPre, spawn.position, Quaternion.Euler(0, 0, 0));
                 bullet.GetComponent<Rigidbody2D>().velocity = distance * bulletSpeed;
+                cannonFire.Play();
+                // Set the object's rotation to the calculated rotation
                 //Destroy(bullet, 3);
             }
 
@@ -81,6 +101,7 @@ public class Cannon : MonoBehaviour
                 SoundManager.Instance.playShootSound();
                 var bulletP2 = Instantiate(bulletPre, spawn.position, Quaternion.Euler(0, 0, 0));
                 bulletP2.GetComponent<Rigidbody2D>().velocity = distanceP2 * bulletSpeed;
+                cannonFire.Play();
                 //Destroy(bulletP2, 3);
             }
 
